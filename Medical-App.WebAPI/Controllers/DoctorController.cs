de5d2186937406e9e6f5892f4fdac67425022a97
unknown <snuwanrasee@gmail.com>
2019-09-11 22:36:48 +0530

    Initial commit

---
+++ Medical-App.WebAPI/Controllers/DoctorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mainframe.MedicalApp.webAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Doctor")]
+    public class DoctorController : Controller
+    {
+    }
+}
